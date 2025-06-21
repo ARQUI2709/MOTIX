f5b7e4a5c74810b4d3e8efdf8c6e9a34c2cf525c
ARQUI2709 <arq.davidospina@gmail.com>
2025-06-20 23:20:54 -0500

    Fix user register

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,4 @@
 import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-// En tu _app.js o layout principal
 import { AuthProvider } from '../contexts/AuthContext';
 
 function MyApp({ Component, pageProps }) {
